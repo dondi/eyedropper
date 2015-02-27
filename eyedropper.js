90b0ca533ce6f5560c705501e739fb05a97da519
dondi <dondi@lmu.edu>
2015-02-26 19:41:40 -0800

    Lay out elements for colors.

--- eyedropper.js
+++ eyedropper.js
@@ -1,8 +1,23 @@
 $(function () {
-    var ctx = $("canvas.eyedropper")[0].getContext('2d');
-    var img = new Image();
+    var $canvas = $("canvas.eyedropper"),
+        ctx = $canvas[0].getContext('2d'),
+
+        loadCanvas = function ($canvas, ctx, image) {
+            $canvas.attr({
+                width: image.width,
+                height: image.height
+            }).css({
+                width: image.width + 'px',
+                height: image.height + 'px'
+            });
+            ctx.drawImage(image, 0, 0);
+        },
+
+        img = new Image();
+
     img.addEventListener('load', function () {
-        ctx.drawImage(img, 0, 0);
+        loadCanvas($canvas, ctx, img);
     }, false);
+
     img.src = 'https://40.media.tumblr.com/a391a1b4b46dd6b498d379e50f96ecbc/tumblr_nkcjuq8Tdr1tnacy1o1_500.jpg';
 });
